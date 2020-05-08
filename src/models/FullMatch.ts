3f6bf100b60d86bca27128eaf9d6f9f3fc161e3e
SofWar <ya.sofwar@yandex.com>
2020-05-08 10:57:14 +0300

    fix

--- src/models/FullMatch.ts
+++ src/models/FullMatch.ts
@@ -23,7 +23,7 @@ export interface FullMatch {
   readonly event: Event
   readonly maps: MapResult[]
   readonly streams: Stream[]
-  readonly sockets: []
+  readonly sockets: any
   readonly demos: Demo[]
   readonly players?: {
     team1: Player[]
